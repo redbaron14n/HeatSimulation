4042148b565794af01bb85e0579033606b627549
redbaron14n <redbaron14n@gmail.com>
2026-06-02 12:48:10 -0500

    Refactor ConductiveSystem.py so attributes may be accessed and changed externally

--- ConductiveSystem.py
+++ ConductiveSystem.py
@@ -15,33 +15,26 @@ class ConductiveSystem1D:
             diff: float,
             cond: float,
             emis: tuple[float, float],
-            tran: tuple[float, float],
+            htcs: tuple[float, float],
             length: float,
-            temp: tuple[float, float, float, float],
+            temps: tuple[float, float, float, float],
         ):
 
         """
         :param float diff: The thermal diffusivity of the material [m^2/s].
         :param float cond: The thermal conductivity of the material [W/m/K].
         :param tuple[float, float] emis: The emissivities of the material at the boundaries [dimensionless].
-        :param tuple[float, float] tran: The heat transfer coefficients at the boundaries [W/m^2/K].
+        :param tuple[float, float] htcs: The heat transfer coefficients at the boundaries [W/m^2/K].
         :param float length: The length of the system [m].
-        :param tuple[float, float, float, float] temp: The surrounding and initial boundary temperatures (surr1, bound1, bound2, surr2) [K].
+        :param tuple[float, float, float, float] temps: The surrounding and initial boundary temperatures (surr1, bound1, bound2, surr2) [K].
         """
 
-        self._set_diffusivity(diff)
-        self._set_conductivity(cond)
-        self._set_emissivity(emis)
-        self._set_heat_transfer_coefs(tran)
-        self._set_length(length)
-        self._set_temps(temp)
-    
-
-    def _set_diffusivity(self, diff: float):
-
-        if diff < 0:
-            raise ValueError("Diffusivity must be non-negative.")
-        self._diffusivity = diff
+        self._diff = diff
+        self._cond = cond
+        self._emis = emis
+        self._htcs = htcs
+        self._length = length
+        self._temps = temps
 
 
     @property
@@ -51,41 +44,71 @@ class ConductiveSystem1D:
         :return: The thermal diffusivity [m^2/s].
         """
 
-        return self._diffusivity
+        return self._diff
+    
+
+    @diffusivity.setter
+    def diffusivity(self, diff: float):
+
+        if diff < 0:
+            raise ValueError("Diffusivity must be non-negative.")
+        self._diff = diff
 
 
-    def _set_conductivity(self, cond: float):
+    @property
+    def conductivity(self) -> float:
+
+        """
+        :return: The thermal conductivity [W/m/K].
+        """
+
+        return self._cond
+
+
+    @conductivity.setter
+    def conductivity(self, cond: float):
 
         if cond < 0:
             raise ValueError("Conductivity must be non-negative.")
-        self._conductivity = cond
+        self._cond = cond
 
 
-    def _set_emissivity(self, emis: tuple[float, float]):
+    @property
+    def emissivities(self) -> tuple[float, float]:
 
-        if len(emis) != 2:
-            raise ValueError("Emissivity input must be a tuple of length 2.")
+        """
+        :return: The emissivities at the boundaries [dimensionless].
+        """
+
+        return self._emissivities
+
+
+    @emissivities.setter
+    def emissivities(self, emis: tuple[float, float]):
+
         for e in emis:
             if e < 0 or e > 1:
                 raise ValueError("Each emissivity must be between 0 and 1 inclusive.")
         self._emissivities = emis
 
 
-    def _set_heat_transfer_coefs(self, tran: tuple[float, float]):
+    @property
+    def heat_transfer_coefs(self) -> tuple[float, float]:
 
-        if len(tran) != 2:
-            raise ValueError("Heat transfer coefficient input must be a tuple of length 2.")
-        for h in tran:
+        """
+        :return: The heat transfer coefficients at the boundaries [W/m^2/K].
+        """
+
+        return self._htcs
+
+
+    @heat_transfer_coefs.setter
+    def heat_transfer_coefs(self, htcs: tuple[float, float]):
+
+        for h in htcs:
             if h < 0:
                 raise ValueError("Each heat transfer coefficient must be non-negative.")
-        self._heat_transfer_coefs = tran
-
-
-    def _set_length(self, length: float):
-
-        if length <= 0:
-            raise ValueError("Length must be positive.")
-        self._length = length
+        self._htcs = htcs
 
 
     @property
@@ -97,12 +120,29 @@ class ConductiveSystem1D:
 
         return self._length
 
-    
-    def _set_temps(self, temp: tuple[float, float, float, float]):
 
-        if len(temp) != 4:
-            raise ValueError("Temperature input must be a tuple of length 4.")
-        for t in temp:
+    @length.setter
+    def length(self, length: float):
+
+        if length <= 0:
+            raise ValueError("Length must be positive.")
+        self._length = length
+
+
+    @property
+    def temperatures(self) -> tuple[float, float, float, float]:
+
+        """
+        :return: The surrounding and initial boundary temperatures (surr1, bound1, bound2, surr2) [K].
+        """
+
+        return self._temps
+
+
+    @temperatures.setter
+    def temperatures(self, temps: tuple[float, float, float, float]):
+
+        for t in temps:
             if t < 0:
                 raise ValueError("Each temperature must be non-negative.")
-        self._temperatures = temp
+        self._temperatures = temps
