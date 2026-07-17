73c20320410881702b1d7b25ceca94948f3ebde9
Ian <redbaron14n@gmail.com>
2026-07-17 11:34:49 -0500

    Added batch file solver to main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from domain.conductive_system_1D import ConductiveSystem1D
 from domain.finite_diff_solver_1D import FiniteDiffSolver1D
 from domain.prefabs import *
+from itertools import product
 from numpy.typing import NDArray
+from traceback import print_exc
 from visualizer import *
 import numpy as np
 
@@ -20,7 +22,8 @@ def run_configuration(
         max_sim_time: float | None = None,
         diff_num: float = 0.1,
         max_t_step: float = 1e-3,
-        force_overwrite: bool = False
+        force_overwrite: bool = False,
+        print_every: int = int(1e5)
 ):
     
     diff, cond, emis = material
@@ -29,9 +32,9 @@ def run_configuration(
         init_temps = np.full(x_res, ambient_temp, dtype=np.float64)
     if max_sim_time is None:
         htc = np.max(htcs_kernel[:, 1])
-        max_sim_time = 5. * cond * length / (diff * htc) + 50. # Based on fitting simulated data + tolerance
+        max_sim_time = 6 * cond * length / (diff * htc) + 200. # Based on fitting simulated data + tolerance
     solver = FiniteDiffSolver1D(system, init_temps, gas_temp_kernel, htcs_kernel, period, ambient_temp, x_res, min_sim_time, max_sim_time, diff_num, max_t_step_size=max_t_step)
-    solver.run_simulation(filename, force_overwrite=force_overwrite)
+    solver.run_simulation(filename, print_every=print_every, force_overwrite=force_overwrite)
 
 
 def get_final_temps(filename: str) -> NDArray[np.float64]:
@@ -49,19 +52,71 @@ def print_reports(data: DataHandler):
     data.report_lag_time()
 
 
-filename = "thurs/hh100_a192n7_k142p1_l12.hdf5"
-htcs_array = np.array([
-    [0., 10., 10.],
-    [0.499999, 10., 10.],
-    [0.5, 100., 10.],
-    [2.999999, 100., 10.]
-], dtype=np.float64)
-material = (4.91e-5, 57.5, np.array([[400., 0.15], [1000., 0.25]], dtype=np.float64))
-run_configuration(filename, material, 0.012, GAS_2200_CONSTANT_ARRAY, htcs_array, period=3.0, diff_num=0.1, max_t_step=1e-4, force_overwrite=False)
+def analyze_data(filename: str):
 
-data = DataHandler(filename)
-data.load_data()
-print_reports(data)
-time_evolution_plot(data)
-front_and_back_temp_plot(data)
-data.close()
+    data = DataHandler(filename)
+    data.load_data()
+    print_reports(data)
+    time_evolution_plot(data)
+    front_and_back_temp_plot(data)
+    data.close()
+
+
+def run_mini(filename: str, htc: float, diff: float, cond: float, length: float, print_every: int=int(1e6)):
+
+    htcs_array = np.array([
+        [0., 10., 10.],
+        [0.499999, 10., 10.],
+        [0.5, htc, 10.],
+        [2.999999, htc, 10.]
+    ], dtype=np.float64)
+    material = (diff, cond, np.array([[0.1, 0.5]], dtype=np.float64))
+    run_configuration(filename, material, length, GAS_2200_CONSTANT_ARRAY, htcs_array, period=3., diff_num=0.1, max_t_step=1e-4, force_overwrite=True, print_every=print_every)
+
+
+def run_batch(htcs: tuple[float, ...], diff_cond_pairs: tuple[tuple[float, float], ...], lengths: tuple[float, ...]):
+
+    failures: list[tuple[int, float, float, float, float]] = []
+    for trial, (htc, diff_cond_pair, length) in enumerate(
+        product(htcs, diff_cond_pairs, lengths)
+    ):
+        # if trial not in [56]:
+        #     continue
+        filename = f"batch/trial{trial}.hdf5"
+        diff, cond = diff_cond_pair
+        try:
+            run_mini(filename, htc, diff, cond, length)
+            data = DataHandler(filename)
+            data.load_data()
+            print_reports(data)
+            data.close()
+        except (ValueError, RuntimeError):
+            failures.append((trial, htc, diff, cond, length))
+            print_exc()
+    print(f"{len(failures)} trials failed.\n{failures}")
+
+
+# filename = "test.hdf5"
+# htcs_array = np.array([
+#     [0., 10., 10.],
+#     [0.499999, 10., 10.],
+#     [0.5, 100., 10.],
+#     [2.999999, 100., 10.]
+# ], dtype=np.float64)
+# material = (4.91e-5, 57.5, np.array([[0.1, 0.5]], dtype=np.float64))
+# run_configuration(filename, material, 0.003, GAS_2200_CONSTANT_ARRAY, htcs_array, period=3.0, diff_num=0.1, max_t_step=1e-4, force_overwrite=False)
+
+htcs = (100., 300., 600.)
+diff_cond_pairs = (
+    (1e-6, 2.),
+    (1.5e-6, 7.),
+    (3e-6, 6.),
+    (3e-6, 20.),
+    (6e-6, 15.),
+    (8e-6, 50.),
+    (2e-5, 40.),
+    (3e-5, 130.),
+    (8e-5, 200.)
+)
+lengths = (0.003, 0.006, 0.012)
+run_batch(htcs, diff_cond_pairs, lengths)
